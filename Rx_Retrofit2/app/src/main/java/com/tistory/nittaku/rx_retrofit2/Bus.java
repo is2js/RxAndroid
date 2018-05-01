6d053e322ea26aeacd0cf724b05a098ae7622925
is2js_notebook <tingstyle1@gmail.com>
2018-05-02 00:59:19 +0900

    11-Rx_Retrofit2 버스정보 받아오기

--- Rx_Retrofit2/app/src/main/java/com/tistory/nittaku/rx_retrofit2/Bus.java
+++ Rx_Retrofit2/app/src/main/java/com/tistory/nittaku/rx_retrofit2/Bus.java
@@ -1,45 +1,50 @@
-
 package com.tistory.nittaku.rx_retrofit2;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
+
+
+
 public class Bus {
 
-        @SerializedName("RESULT")
-        @Expose
-        private RESULT rESULT;
-        @SerializedName("BUSSTOP_LIST")
-        @Expose
-        private List<Object> bUSSTOPLIST = null;
-        @SerializedName("ROW_COUNT")
-        @Expose
-        private Integer rOWCOUNT;
+    @SerializedName("RESULT")
+    @Expose
+    private RESULT rESULT;
+    @SerializedName("BUSSTOP_LIST")
+    @Expose
+    private List<BUSSTOPLIST> bUSSTOPLIST = null;
+    @SerializedName("ROW_COUNT")
+    @Expose
+    private Integer rOWCOUNT;
 
-        public RESULT getRESULT() {
-            return rESULT;
-        }
+    public RESULT getRESULT() {
+        return rESULT;
+    }
 
-        public void setRESULT(RESULT rESULT) {
-            this.rESULT = rESULT;
-        }
+    public void setRESULT(RESULT rESULT) {
+        this.rESULT = rESULT;
+    }
 
-        public List<Object> getBUSSTOPLIST() {
-            return bUSSTOPLIST;
-        }
+    public List<BUSSTOPLIST> getBUSSTOPLIST() {
+        return bUSSTOPLIST;
+    }
 
-        public void setBUSSTOPLIST(List<Object> bUSSTOPLIST) {
-            this.bUSSTOPLIST = bUSSTOPLIST;
-        }
+    public void setBUSSTOPLIST(List<BUSSTOPLIST> bUSSTOPLIST) {
+        this.bUSSTOPLIST = bUSSTOPLIST;
+    }
 
-        public Integer getROWCOUNT() {
-            return rOWCOUNT;
-        }
+    public Integer getROWCOUNT() {
+        return rOWCOUNT;
+    }
 
-        public void setROWCOUNT(Integer rOWCOUNT) {
-            this.rOWCOUNT = rOWCOUNT;
-        }
+    public void setROWCOUNT(Integer rOWCOUNT) {
+        this.rOWCOUNT = rOWCOUNT;
+    }
+
+
 
 
     public class RESULT {
@@ -66,6 +71,165 @@ public class Bus {
         public void setRESULTCODE(String rESULTCODE) {
             this.rESULTCODE = rESULTCODE;
         }
+    }
+
+    public class BUSSTOPLIST {
+
+        @SerializedName("ARRIVE")
+        @Expose
+        private String aRRIVE;
+        @SerializedName("REMAIN_MIN")
+        @Expose
+        private Integer rEMAINMIN;
+        @SerializedName("ENG_BUSSTOP_NAME")
+        @Expose
+        private String eNGBUSSTOPNAME;
+        @SerializedName("REMAIN_STOP")
+        @Expose
+        private Integer rEMAINSTOP;
+        @SerializedName("DIR_START")
+        @Expose
+        private String dIRSTART;
+        @SerializedName("BUS_ID")
+        @Expose
+        private String bUSID;
+        @SerializedName("BUSSTOP_NAME")
+        @Expose
+        private String bUSSTOPNAME;
+        @SerializedName("DIR_END")
+        @Expose
+        private String dIREND;
+        @SerializedName("DIR_PASS")
+        @Expose
+        private String dIRPASS;
+        @SerializedName("LOW_BUS")
+        @Expose
+        private Integer lOWBUS;
+        @SerializedName("CURR_STOP_ID")
+        @Expose
+        private Integer cURRSTOPID;
+        @SerializedName("ARRIVE_FLAG")
+        @Expose
+        private Integer aRRIVEFLAG;
+        @SerializedName("LINE_ID")
+        @Expose
+        private Integer lINEID;
+        @SerializedName("LINE_NAME")
+        @Expose
+        private String lINENAME;
+
+        public String getARRIVE() {
+            return aRRIVE;
+        }
+
+        public void setARRIVE(String aRRIVE) {
+            this.aRRIVE = aRRIVE;
+        }
+
+        public Integer getREMAINMIN() {
+            return rEMAINMIN;
+        }
+
+        public void setREMAINMIN(Integer rEMAINMIN) {
+            this.rEMAINMIN = rEMAINMIN;
+        }
+
+        public String getENGBUSSTOPNAME() {
+            return eNGBUSSTOPNAME;
+        }
+
+        public void setENGBUSSTOPNAME(String eNGBUSSTOPNAME) {
+            this.eNGBUSSTOPNAME = eNGBUSSTOPNAME;
+        }
+
+        public Integer getREMAINSTOP() {
+            return rEMAINSTOP;
+        }
+
+        public void setREMAINSTOP(Integer rEMAINSTOP) {
+            this.rEMAINSTOP = rEMAINSTOP;
+        }
+
+        public String getDIRSTART() {
+            return dIRSTART;
+        }
+
+        public void setDIRSTART(String dIRSTART) {
+            this.dIRSTART = dIRSTART;
+        }
+
+        public String getBUSID() {
+            return bUSID;
+        }
+
+        public void setBUSID(String bUSID) {
+            this.bUSID = bUSID;
+        }
+
+        public String getBUSSTOPNAME() {
+            return bUSSTOPNAME;
+        }
+
+        public void setBUSSTOPNAME(String bUSSTOPNAME) {
+            this.bUSSTOPNAME = bUSSTOPNAME;
+        }
+
+        public String getDIREND() {
+            return dIREND;
+        }
+
+        public void setDIREND(String dIREND) {
+            this.dIREND = dIREND;
+        }
+
+        public String getDIRPASS() {
+            return dIRPASS;
+        }
+
+        public void setDIRPASS(String dIRPASS) {
+            this.dIRPASS = dIRPASS;
+        }
+
+        public Integer getLOWBUS() {
+            return lOWBUS;
+        }
+
+        public void setLOWBUS(Integer lOWBUS) {
+            this.lOWBUS = lOWBUS;
+        }
+
+        public Integer getCURRSTOPID() {
+            return cURRSTOPID;
+        }
+
+        public void setCURRSTOPID(Integer cURRSTOPID) {
+            this.cURRSTOPID = cURRSTOPID;
+        }
+
+        public Integer getARRIVEFLAG() {
+            return aRRIVEFLAG;
+        }
+
+        public void setARRIVEFLAG(Integer aRRIVEFLAG) {
+            this.aRRIVEFLAG = aRRIVEFLAG;
+        }
+
+        public Integer getLINEID() {
+            return lINEID;
+        }
+
+        public void setLINEID(Integer lINEID) {
+            this.lINEID = lINEID;
+        }
+
+        public String getLINENAME() {
+            return lINENAME;
+        }
+
+        public void setLINENAME(String lINENAME) {
+            this.lINENAME = lINENAME;
+        }
 
     }
+
 }
--- Rx_Retrofit2/app/src/main/java/com/tistory/nittaku/rx_retrofit2/MainActivity.java
+++ Rx_Retrofit2/app/src/main/java/com/tistory/nittaku/rx_retrofit2/MainActivity.java
@@ -2,13 +2,16 @@ package com.tistory.nittaku.rx_retrofit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import com.jakewharton.rxbinding2.view.RxView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
@@ -18,14 +21,14 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    //static final String BASE_URL = "http://api.openweathermap.org";
-    //static final String API_KEY = "11c7f631cd74614447645fc773e2a1a7";
+//    http://bis.naju.go.kr:8080/json/arriveAppInfo?BUSSTOP_ID=1439
     static final String BASE_URL = "http://bis.naju.go.kr:8080/";
     String bus;
 
     EditText busnumber;
     TextView result;
     Button btn_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,24 +55,54 @@ public class MainActivity extends AppCompatActivity {
         Retrofit2 service = client.create(Retrofit2.class);
 
         // 3. 데이터 Observable 생성
-        Observable<Bus> weatherData = service.getData( busstop_Id );
+
 
         // 4. scribeOn
         // 가. 데이터를 가져오는 대상 Observable : newTHread 로 새로운 Thread 에서 작업한다.
         // 나. 화면에 새팅하는 Observer : main Thread에서 작업한다.
-
-        weatherData.subscribeOn(Schedulers.newThread())
+        Observable<Bus> BusData = service.getData( busstop_Id );
+        BusData.subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         data -> {
                             String temp = "";
                             temp += "RESULTMSG:" + data.getRESULT().getRESULTMSG();
                             temp += "\nRESULTCODE:" + data.getRESULT().getRESULTCODE();
-                            temp += "\nBUSTOPLIST : "+ data.getBUSSTOPLIST();
                             temp += "\nROWCOUNT:" + data.getROWCOUNT();
                             result.setText(temp);
                         }
+                        ,e-> Toast.makeText(this, "인터넷이 연결되지 않았어요", Toast.LENGTH_SHORT).show()
+                        ,()->{Toast.makeText(this, "정상적으로 버스정보를 가져왔습니다.", Toast.LENGTH_SHORT).show(); }
                 );
+
+
+        BusData.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe( item->
+                        {
+                            if (item.getRESULT().getRESULTCODE().equals("SUCCESS")) {
+                                Toast.makeText(this, "버스도착정보가 있음", Toast.LENGTH_SHORT).show();
+                                StringBuffer bf = new StringBuffer();
+
+
+                                Observable.fromIterable( item.getBUSSTOPLIST() ) //json속 배열을 가진 놈은 fromIterable로 쪼개어서 쓴다!!
+                                        .subscribe(list ->
+                                                {
+                                                    bf. append("<< "+list.getLINENAME().toString()+" >> 버스가 \n" );
+                                                    bf. append("도착까지" + list.getREMAINMIN().toString()+"분 ("+list.getREMAINSTOP().toString()+"개 정류장) 남았어요\n" );
+                                                    bf. append("현재버스 위치는 " + list.getBUSSTOPNAME().toString()+"정류장 이에요.\n" );
+
+                                                    ((TextView) findViewById(R.id.txt_result2)).setText(bf);
+                                                }
+                                        );
+                            } else ((TextView) findViewById(R.id.txt_result2)).setText("버스도착정보가 없음");
+                        }
+                        , e-> Toast.makeText(this, "인터넷이 연결되지 않았어요", Toast.LENGTH_SHORT).show()
+                        , ()->{ Toast.makeText(this, "정상적으로 버스정보를 가져왔습니다.", Toast.LENGTH_SHORT).show(); }
+                        );
+
+
+
 
     }
 }
